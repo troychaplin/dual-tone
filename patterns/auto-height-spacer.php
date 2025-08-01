e5fadcf10ef555800d428617a9df7428b748210e
Troy Chaplin <troy.chaplin@gmail.com>
2025-08-01 07:55:07 -0400

    add: single post template

---
+++ patterns/auto-height-spacer.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Title: Auto Height Spacer
+ * Slug: single-tone/auto-height-spacer
+ * Categories: Utilities
+ */
+?>
+<!-- wp:spacer {"style":{"layout":{"selfStretch":"fill","flexSize":null}}} -->
+<div aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
