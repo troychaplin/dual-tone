a61d8e3270b79b4f99a6f81ece4b99a99400e571
Troy Chaplin <troy.chaplin@gmail.com>
2025-08-02 08:44:53 -0400

    add: archive and card pattern

---
+++ patterns/post-card.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Title: Post Card
+ * Slug: dual-tone/post-card
+ * Categories: Template Parts
+ */
+?>
+<!-- wp:group {"metadata":{"name":"Card"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","right":"2rem","bottom":"var:preset|spacing|50","left":"2rem"},"blockGap":"0"},"border":{"radius":"4px","width":"1px","color":"#d5d9f0"},"dimensions":{"minHeight":"100%"}},"backgroundColor":"custom-white","layout":{"type":"flex","orientation":"vertical","justifyContent":"left"}} -->
+<div class="wp-block-group has-border-color has-custom-white-background-color has-background" style="border-color:#d5d9f0;border-width:1px;border-radius:4px;min-height:100%;padding-top:var(--wp--preset--spacing--50);padding-right:2rem;padding-bottom:var(--wp--preset--spacing--50);padding-left:2rem"><!-- wp:group {"metadata":{"name":"Content"},"style":{"layout":{"selfStretch":"fill","flexSize":null},"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:post-date {"style":{"spacing":{"margin":{"bottom":"0"}},"typography":{"fontStyle":"italic","fontWeight":"300"}},"fontSize":"small"} /-->
+
+<!-- wp:post-title {"level":3,"isLink":true,"style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-scampi-600"},":hover":{"color":{"text":"var:preset|color|custom-scampi-900"}}}},"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"margin":{"bottom":"0","top":"0.3rem"}},"typography":{"fontStyle":"normal","fontWeight":"400"},"border":{"bottom":{"style":"none","width":"0px"}}},"fontSize":"large","fontFamily":"exo-2"} /-->
+
+<!-- wp:post-excerpt {"excerptLength":20,"fontSize":"medium"} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:post-terms {"term":"category","prefix":"Categorized in ","style":{"typography":{"fontStyle":"italic","fontWeight":"300"},"spacing":{"margin":{"top":"var:preset|spacing|60"},"padding":{"top":"0","bottom":"0","left":"0","right":"0"}},"layout":{"selfStretch":"fit","flexSize":null},"border":{"radius":"4px"},"elements":{"link":{"color":{"text":"var:preset|color|custom-scampi-700"},":hover":{"color":{"text":"var:preset|color|custom-scampi-950"}}}}},"textColor":"custom-scampi-700","fontSize":"small","fontFamily":"lato"} /--></div>
+<!-- /wp:group -->
